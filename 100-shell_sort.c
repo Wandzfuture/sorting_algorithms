779556d1990722790301f209ec5ab740c909a810
Wandzfuture <michael.zabas@gmail.com>
2024-02-20 20:47:56 +0000

    0x1B. C - Sorting algorithms & Big O

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,28 +8,26 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, j;
+	size_t i, j, gap;
 	int temp;
 
-	while (gap < size / 3)
-		gap = gap *3 + 1;
+	for (gap = 1; gap < size / 3; gap = gap *3 + 1)
+	{;
+	}
 
-	while (gap > 0)
+	for (; gap > 0; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
-			j = i;
-			while (j >= gap && array[j - gap] > temp)
+
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 			{
 				array[j] = array[j - gap];
-				j -= gap;
 			}
 
 			array[j] = temp;
+			print_array(array, size);
 		}
-
-		print_array(array, size);
-		gap /= 3;
 	}
 }
